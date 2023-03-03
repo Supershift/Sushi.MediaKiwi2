5e0f0f79c887628fd7272ac1343e170af370cd15
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-03-03 16:43:22 +0100

    Fixed internal compile error

--- src/Sushi.MediaKiwi.Vue/src/components/MkTable/MkTableFilterTextField.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkTable/MkTableFilterTextField.vue
@@ -1,23 +1,20 @@
 <script setup lang="ts">
-import type { ITableFilterItem, ITableFilterValue } from '@/models/table/';
+  import type { ITableFilterItem, ITableFilterValue } from "@/models/table";
 
-const props = defineProps<{
-    tableFilterItem: ITableFilterItem,
-    modelValue: ITableFilterValue
-}>();
+  const props = defineProps<{
+    tableFilterItem: ITableFilterItem;
+    modelValue: ITableFilterValue;
+  }>();
 
-const emit = defineEmits<{  
-  (e: 'update:modelValue', value: ITableFilterValue): void
-}>()
+  const emit = defineEmits<{
+    (e: "update:modelValue", value: ITableFilterValue): void;
+  }>();
 
-function textFieldChanged(selectedValue: string) {
-    emit('update:modelValue', { title: selectedValue, value: selectedValue });
-}
-
+  function textFieldChanged(selectedValue: string) {
+    emit("update:modelValue", { title: selectedValue, value: selectedValue });
+  }
 </script>
 
-<template>    
-    <v-text-field 
-        :value="modelValue?.value" @update:model-value="textFieldChanged" label="Value">
-    </v-text-field>
+<template>
+  <v-text-field :value="modelValue?.value" @update:model-value="textFieldChanged" label="Value"> </v-text-field>
 </template>
