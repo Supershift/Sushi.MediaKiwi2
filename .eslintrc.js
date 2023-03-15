8b05a90c816cc4312c9abf2abb0e2d2a48ca6eff
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-03-15 14:35:32 +0100

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+/**
+ * Typescript Options: https://typescript-eslint.io/rules/consistent-type-exports/
+ */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: ["*.{j,t}s?(x)"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/consistent-type-definitions": "error", // Typescript recommends using interfaces over types: https://www.typescriptlang.org/play#example/types-vs-interfaces
+      },
+    },
+  ],
+};
