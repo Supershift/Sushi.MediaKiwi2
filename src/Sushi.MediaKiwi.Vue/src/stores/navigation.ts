b353ce1eb068a71a4af4f281edf9441937701b70
Luigi Arends <luigi.arends@supershift.nl>
2023-03-29 15:20:14 +0200

    navigate to new default

--- src/Sushi.MediaKiwi.Vue/src/stores/navigation.ts
+++ src/Sushi.MediaKiwi.Vue/src/stores/navigation.ts
@@ -40,23 +40,27 @@ export const useNavigationStore = defineStore({
       }
     },
     setSectionNavigationItems(name: string) {
+      // check if this is a valid Section and assign the right sub-items to that section
       const currentSectionId = useMediakiwiStore().mediakiwiSections.find((x) => x.name === name)?.id;
-      console.log(name, currentSectionId);
       if (name) {
         if (currentSectionId !== undefined) {
+          //assign the right navigation items to the section
           const items = useMediakiwiStore().mediakiwiNavigationItems.filter((x) => x.sectionId === currentSectionId);
-          console.log(items);
 
           if (items && items.length) {
             this.navigationItems = items;
           }
         } else {
+          // not a section, so assign somewhere to land fetch the right items
           const currentNavigationItem = useMediakiwiStore().mediakiwiNavigationItems.find((x) => x.name === name);
           const items = getNavigationItemChildren(currentNavigationItem, useMediakiwiStore().mediakiwiNavigationItems, true);
           this.navigationItems = items;
         }
       } else {
+        // there is no section selected so lets just place navigation items to empty for now
+        const defaultSection = useMediakiwiStore().mediakiwiSections.at(0);
         this.navigationItems = [];
+        this.router.push({ name: defaultSection?.name });
       }
     },
     setBreadCrumbs(breadcrumbs: Array<IBreadcrumb>) {
