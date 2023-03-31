ce26b3f3ceac8d8e07ed23fb5a593c7aa0d24dd9
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-03-31 11:50:54 +0200

    Update MkTableCellIcon.vue

--- src/Sushi.MediaKiwi.Vue/src/components/MkTable/MkTableCellIcon.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkTable/MkTableCellIcon.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { ref, computed } from "vue";
+  import { ref } from "vue";
   import { ITableMapItemIconOptions } from "@/models/table/ITableMapItemIconOptions";
   import { IconPosition } from "@/models";
 
