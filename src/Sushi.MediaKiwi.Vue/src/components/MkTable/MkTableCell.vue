ae2771b6e87f11c9c62893ea1d4ea9bae2ea9ceb
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-03-31 10:44:05 +0200

    Renamed MkTableCellIcon to MkTableCellValue

--- src/Sushi.MediaKiwi.Vue/src/components/MkTable/MkTableCell.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkTable/MkTableCell.vue
@@ -2,7 +2,7 @@
   import { ref, computed } from "vue";
   import { IconPosition, ITableMapItemIconOptions } from "@/models";
   import type { ITableMapItem } from "@/models/table/ITableMapItem";
-  import MkTableCellIcon from "./MkTableCellIcon.vue";
+  import MkTableCellValue from "./MkTableCellValue.vue";
   import { IconOptions } from "vuetify/lib/framework.mjs";
 
   const props = defineProps<{
@@ -31,25 +31,25 @@
     <template v-else>
       <!-- render a boolean -->
       <template v-if="isBooleanValue">
-        <MkTableCellIcon :icon-options="{ value: () => (invokeMapItemValue(props.mapItem) ? 'mdi-check-circle-outline' : 'mdi-close-circle-outline') }" />
+        <MkTableCellValue :icon-options="{ value: () => (invokeMapItemValue(props.mapItem) ? 'mdi-check-circle-outline' : 'mdi-close-circle-outline') }" />
       </template>
 
       <!-- render any other value -->
       <template v-else>
         <!-- optionally render icon -->
-        <MkTableCellIcon :data="data" :icon-options="mapItem.iconOptions">
+        <MkTableCellValue :data="data" :icon-options="mapItem.iconOptions">
           <template v-if="mapItem.iconOptions?.position === IconPosition.behind" #in-front-icon>
             {{ invokeMapItemValue(mapItem) }}
           </template>
           <template v-else #behind-icon>
             {{ invokeMapItemValue(mapItem) }}
           </template>
-        </MkTableCellIcon>
+        </MkTableCellValue>
 
         <!-- <label></label> -->
 
         <!-- render icon behind -->
-        <!-- <MkTableCellIcon v-if="mapItem.iconOptions && mapItem.iconOptions.position === IconPosition.behind" :data="data" :icon-options="mapItem.iconOptions" /> -->
+        <!-- <MkTableCellValue v-if="mapItem.iconOptions && mapItem.iconOptions.position === IconPosition.behind" :data="data" :icon-options="mapItem.iconOptions" /> -->
       </template>
     </template>
   </td>
