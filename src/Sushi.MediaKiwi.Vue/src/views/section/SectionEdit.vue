ec6ae8fb315df09800ebba98a6de94d6554c0a1b
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-04-19 14:33:20 +0200

    Removed unused import

--- src/Sushi.MediaKiwi.Vue/src/views/section/SectionEdit.vue
+++ src/Sushi.MediaKiwi.Vue/src/views/section/SectionEdit.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
   import MkSectionEdit from "@/components/MkSection/MkSectionEdit.vue";
-  import { MkSuspense } from "@/components";
 </script>
 <template>
   <MkSectionEdit />
--- src/Sushi.MediaKiwi.Vue/src/views/section/SectionOverview.vue
+++ src/Sushi.MediaKiwi.Vue/src/views/section/SectionOverview.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
   import MkSectionOverview from "@/components/MkSection/MkSectionOverview.vue";
-  import { MkSuspense } from "@/components";
 </script>
 <template>
   <MkSectionOverview />
