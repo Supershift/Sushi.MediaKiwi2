e6b021ca650de0044e7e565045ce1324782223c2
Mark de Vries <mark.devries@supershift.nl>
2023-04-20 12:59:41 +0200

    added route record for root '/'

--- src/Sushi.MediaKiwi.Vue/src/identity/isAuthenticated.ts
+++ src/Sushi.MediaKiwi.Vue/src/identity/isAuthenticated.ts
@@ -1,19 +1,25 @@
 import { InteractionType, PopupRequest, PublicClientApplication, RedirectRequest } from "@azure/msal-browser";
+import { identity } from "@/identity";
+export function isAuthenticated(): boolean {
+  const account = identity.msalInstance.getActiveAccount();
+  if (account) {
+    return true;
+  } else return false;
+}
 
 /** Checks if a user is authenticated. If not, the user is redirected to login. */
-export async function isAuthenticated(instance: PublicClientApplication, interactionType: InteractionType, loginRequest: PopupRequest | RedirectRequest): Promise<boolean> {
+export async function tryIsAuthenticated(interactionType: InteractionType, loginRequest: PopupRequest | RedirectRequest): Promise<boolean> {
   // If your application uses redirects for interaction, handleRedirectPromise must be called and awaited on each page load before determining if a user is signed in or not
-  return instance
+  return identity.msalInstance
     .handleRedirectPromise()
-    .then(() => {
-      const accounts = instance.getAllAccounts();
-      if (accounts.length > 0) {
+    .then(() => {      
+      if (isAuthenticated()) {
         return true;
       }
 
       // User is not signed in and attempting to access protected route. Sign them in.
       if (interactionType === InteractionType.Popup) {
-        return instance
+        return identity.msalInstance
           .loginPopup(loginRequest)
           .then(() => {
             return true;
@@ -22,7 +28,7 @@ export async function isAuthenticated(instance: PublicClientApplication, interac
             return false;
           });
       } else if (interactionType === InteractionType.Redirect) {
-        return instance
+        return identity.msalInstance
           .loginRedirect(loginRequest)
           .then(() => {
             return true;
