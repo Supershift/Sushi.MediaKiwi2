b5d8df641b254971845de3d208b73131d909d618
Mark de Vries <mark.devries@supershift.nl>
2023-05-02 10:36:18 +0200

    register route for root, e.g. www.mysite.com

--- src/Sushi.MediaKiwi.Vue/src/router/getDefaultRouterOptions.ts
+++ src/Sushi.MediaKiwi.Vue/src/router/getDefaultRouterOptions.ts
@@ -1,10 +1,6 @@
 import { createWebHistory, type RouteRecordRaw, type RouterOptions } from "vue-router";
 import SignIn from "@/views/SignIn.vue";
 import LoginRedirect from "@/views/LoginRedirect.vue";
-import Home from "@/views/Home.vue";
-import { identity } from "@/identity";
-import { isAuthenticated } from "../identity/isAuthenticated";
-import { InteractionType } from "@azure/msal-browser";
 
 /** Creates default router options based on provided modules. */
 export function getDefaultRouterOptions(customRoutes?: RouteRecordRaw[]): RouterOptions {
@@ -19,12 +15,11 @@ export function getDefaultRouterOptions(customRoutes?: RouteRecordRaw[]): Router
   // add sign in screen
   routes.push({ path: "/signIn", component: SignIn });
   routes.push({ path: "/loginRedirect", component: LoginRedirect });
-  routes.push({ path: "/home", component: Home, meta: { requiresAuth: true } });
   routes.push({
+    name: "root",
     path: "/",
     redirect: (to) => {
-      // if the user is authenticated, redirect to home, else redirect to sign in
-      return isAuthenticated() ? "/home" : "signIn";
+      return "/signIn";
     },
   });
 
