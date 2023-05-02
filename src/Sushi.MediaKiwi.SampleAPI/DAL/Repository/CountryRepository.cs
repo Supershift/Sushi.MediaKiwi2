b88dd01c0cf84171aeba015c63af0c134d5eaab3
Mark de Vries <mark.devries@supershift.nl>
2023-05-02 13:23:53 +0200

    added countries to sample API

---
+++ src/Sushi.MediaKiwi.SampleAPI/DAL/Repository/CountryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Sushi.MediaKiwi.DAL.Paging;
+using Sushi.MicroORM;
+
+namespace Sushi.MediaKiwi.SampleAPI.DAL.Repository
+{
+    public class CountryRepository
+    {
+        private readonly IConnector<Country> _connector;
+
+        public CountryRepository(IConnector<Country> connector)
+        {
+            _connector = connector;
+        }
+
+        public async Task<QueryListResult<Country>> GetAllAsync(PagingValues pagingValues)
+        {
+            var query = _connector.CreateQuery();
+            query.AddPaging(pagingValues);
+            query.AddOrder(x => x.Name);
+            var result = await _connector.GetAllAsync(query);
+            return result;
+        }
+    }
+}
---
+++ src/Sushi.MediaKiwi.SampleAPI/Service/Model/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sushi.MediaKiwi.SampleAPI.Service.Model
+{
+    public class Country
+    {
+        /// <summary>
+        /// ISO country code
+        /// </summary>
+        [Required, StringLength(2, MinimumLength = 2)]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Name of the country
+        /// </summary>
+        [Required, StringLength(128)]
+        public string Name { get; set; }
+    }
+}
