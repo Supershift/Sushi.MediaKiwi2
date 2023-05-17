8f323932b68fb9d72cb39662f27453f6e8f837fc
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-05-17 14:46:52 +0200

    Added avatar in app-bar with popover menu

--- src/Sushi.MediaKiwi.Vue/src/components/MkIdentity/MkAvatar.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkIdentity/MkAvatar.vue
@@ -1,14 +1,47 @@
-<script setup lang="ts">  
+<script setup lang="ts">
   import { useMsal } from "@/composables/useMsal";
   import { useIsAuthenticated } from "@/composables/useIsAuthenticated";
+  import { MkSignOutButton, MkLanguageSwitch } from "@/components";
+  import { ref } from "vue";
 
-  const { instance, account } = useMsal();
+  const { account } = useMsal();
+
+  const menu = ref(false);
 
   const isAuthenticated = useIsAuthenticated();
 </script>
 
 <template>
   <span v-if="isAuthenticated">
-    <VContainer> {{ account?.username }} </VContainer>
+    <div class="text-center">
+      <v-menu v-model="menu" :close-on-content-click="false" location="bottom">
+        <template #activator="{ props }">
+          <v-container>
+            <v-avatar color="surface" v-bind="props" :title="account?.username">
+              <v-icon icon="mdi-account-circle"></v-icon>
+            </v-avatar>
+          </v-container>
+        </template>
+
+        <v-card min-width="300">
+          <v-list>
+            <v-container> {{ account?.username }} </v-container>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item>
+              <mk-language-switch></mk-language-switch>
+            </v-list-item>
+          </v-list>
+
+          <v-card-actions>
+            <mk-sign-out-button></mk-sign-out-button>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </div>
   </span>
 </template>
