10e76f1f339de5250e0b8266591c1aa6415ea9b7
Mark de Vries <mark.devries@supershift.nl>
2023-05-30 11:06:39 +0200

    added inline help to tableCellIcon type

--- src/Sushi.MediaKiwi.Vue/src/models/table/TableCellIcon.ts
+++ src/Sushi.MediaKiwi.Vue/src/models/table/TableCellIcon.ts
@@ -4,8 +4,12 @@ export enum TableIconPosition {
 }
 
 export interface TableCellIcon {
+  /** Defines the position of the icon relative to the label */
   position: TableIconPosition;
+  /** Sets the icon to display based on mdi icon names */
   iconName: string;
+  /** Text value to display in the cell */
   label?: string;
+  /** Tooltip to display when hovering over the icon */
   tooltip?: string;
 }
