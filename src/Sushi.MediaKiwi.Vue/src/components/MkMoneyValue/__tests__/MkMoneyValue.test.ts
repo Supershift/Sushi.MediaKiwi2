5e7f2dd945bca1a267fae86dcb044dc5eb85f4ff
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-06-23 10:33:26 +0200

    Created proper test for MkMoneyValue

--- src/Sushi.MediaKiwi.Vue/src/components/MkMoneyValue/__tests__/MkMoneyValue.test.ts
+++ src/Sushi.MediaKiwi.Vue/src/components/MkMoneyValue/__tests__/MkMoneyValue.test.ts
@@ -1,60 +1,74 @@
+/* eslint-disable no-unused-labels */
 import "reflect-metadata";
-import { RenderOptions, render } from "@testing-library/vue";
-import MkMoneyValue from "../MkMoneyValue.vue";
-import { describe, it, expect, vi, beforeEach } from "vitest";
-import i18next from "i18next";
-import { defineComponent } from "vue";
-import { h, Suspense } from "vue";
+import MkMoneyValueComponent from "../MkMoneyValue.vue";
+import { describe, expect, vi, beforeEach } from "vitest";
+import { container } from "tsyringe";
+import { mountAsync } from "@tests/helpers/mount";
 
 // mock libraries
+vi.mock("@azure/msal-browser");
 vi.mock("i18next");
-
-const renderAsync = async (component: any, options?: RenderOptions | undefined) => {
-  const wrapper = render(
-    defineComponent({
-      render() {
-        return h(Suspense, null, {
-          default: h(component, options?.props),
-          fallback: h("div", "fallback"),
-        });
-      },
-    }),
-    options
-  );
-
-  // await flushPromises();
-  return wrapper;
-};
+vi.mock("vue-router");
+vi.mock("vuetify");
 
 describe("MkMoneyValue", () => {
+  // Declare the props to be used in all tests
+  const props = {
+    label: "SRP",
+    modelValue: {
+      currency: "EUR",
+      amount: 12.5,
+    },
+  };
+
   beforeEach(() => {
+    container.reset();
     vi.clearAllMocks();
   });
 
-  describe("Render", () => {
-    test("it should work", async () => {
-      const { getByText } = await renderAsync(MkMoneyValue, {
-        props: {
-          label: "SRP",
-          value: {
-            currency: "USD",
-            amount: 100,
-          },
-        },
-        global: {
-          provide: {
-            i18next,
-            i18initPromise: i18next, //createInstance().init(),
-          },
-          mocks: {
-            $t: (key: string) => key,
-          },
-        },
-      });
+  it("Should be rendered", async () => {
+    // Mount the component in a suspense wrapper
+    const wrapper = await mountAsync(MkMoneyValueComponent, props);
 
-      // assert output
-      const element = getByText("SRP");
-      expect(element).not.toBeNull();
+    // Check that the component is rendered and contains a v-input
+    const outputHtml = wrapper.html();
+    expect(outputHtml).not.toBeNull();
+    expect(outputHtml).not.toBeUndefined();
+    expect(outputHtml).not.toEqual("");
+    expect(outputHtml).toContain("v-input");
+  });
+
+  describe("Props", async () => {
+    it("Label is set", async () => {
+      // Mount the component in a suspense wrapper
+      const wrapper = await mountAsync(MkMoneyValueComponent, props);
+
+      // Check that the label is rendered
+      expect(wrapper.find(".v-input__control").text()).toContain("SRP");
+    });
+
+    it("Currency is set", async () => {
+      // Mount the component in a suspense wrapper
+      const wrapper = await mountAsync(MkMoneyValueComponent, props);
+
+      // Get the element that should hold the currency value
+      const element = wrapper.find(".v-autocomplete__selection .v-autocomplete__selection-text");
+
+      // Check that the element is rendered and contains the currency
+      expect(element).toBeDefined();
+      expect(element.text()).toContain("EUR");
+    });
+
+    it("Input value is set", async () => {
+      // Mount the component in a suspense wrapper
+      const wrapper = await mountAsync(MkMoneyValueComponent, props);
+
+      // Get the input element and cast it to HTMLInputElement to access the value
+      const inputElement = wrapper.find(".v-field__field input[type='number'].v-field__input").element as HTMLInputElement;
+
+      // Check that the element is rendered and contains the currency
+      expect(inputElement).toBeDefined();
+      expect(inputElement.value).toContain("12.5");
     });
   });
 });
