36a1fa225b2fddc489a24a025e7a08758525c25b
Luigi Arends <luigi.arends@supershift.nl>
2023-07-04 09:27:56 +0200

    added info example

--- src/Sushi.MediaKiwi.SampleWeb/ClientApp/src/views/Countries.vue
+++ src/Sushi.MediaKiwi.SampleWeb/ClientApp/src/views/Countries.vue
@@ -21,6 +21,8 @@
   const example_radio = ref("name");
   const example_range = ref(0);
   const example_select = ref(["name", "code"]);
+  const example_selection = ref(1);
+  const example_loading = ref(false);
 
   // define mapping
   const tableMap: TableMap<Country> = {
@@ -45,6 +47,11 @@
   }
   async function close() {
     toggle.value = false;
+  }
+  async function reserve() {
+    example_loading.value = true;
+
+    setTimeout(() => (example_loading.value = false), 2000);
   }
 </script>
 <template>
@@ -80,9 +87,13 @@
         </v-list-item>
         END
       </v-list>
+      <!-- End DEMO: List -->
+      <!-- Start DEMO: Table -->
       <v-card v-if="exampleChoice === 'table-view'">
         <mk-table v-model:current-page="currentPage" :api-result="countries" :table-map="tableMap" :on-load="LoadData" @click:row="RowClicked"></mk-table>
       </v-card>
+      <!-- End DEMO: Table -->
+      <!-- Start DEMO: Filters -->
       <v-card v-if="exampleChoice === 'filters-view'">
         <v-list disabled>
           <v-list-subheader>Include</v-list-subheader>
@@ -110,7 +121,57 @@
           </v-list-item>
         </v-list>
       </v-card>
-      <!-- End DEMO: List -->
+      <!-- End DEMO: Filters -->
+      <!-- Start DEMO: Info -->
+      <v-card v-if="exampleChoice === 'info-view'" :loading="example_loading" class="mx-auto" max-width="374">
+        <template #loader="{ isActive }">
+          <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
+        </template>
+
+        <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+        <v-card-item>
+          <v-card-title>Cafe Badilico</v-card-title>
+
+          <v-card-subtitle>
+            <span class="me-1">Local Favorite</span>
+
+            <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
+          </v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating :model-value="4.5" color="amber" density="compact" half-increments readonly size="small"></v-rating>
+
+            <div class="text-grey ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+        </v-card-text>
+
+        <v-divider class="mx-4 mb-1"></v-divider>
+
+        <v-card-title>Tonight's availability</v-card-title>
+
+        <div class="px-4">
+          <v-chip-group v-model="example_selection">
+            <v-chip>5:30PM</v-chip>
+
+            <v-chip>7:30PM</v-chip>
+
+            <v-chip>8:00PM</v-chip>
+
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </div>
+
+        <v-card-actions>
+          <v-btn color="deep-purple-lighten-2" variant="text" @click="reserve"> Reserve </v-btn>
+        </v-card-actions>
+      </v-card>
     </template>
     <template #footer>
       <!-- <v-btn class="d-inline-block" variant="text">action 1</v-btn>
