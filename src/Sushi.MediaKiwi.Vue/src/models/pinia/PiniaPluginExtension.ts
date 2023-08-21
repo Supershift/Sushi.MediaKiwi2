4919053cf8b78bb3f337c9b1584768279632bd97
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-08-21 13:24:29 +0200

    Fixed qute and whitespace eslint errors

--- src/Sushi.MediaKiwi.Vue/src/models/pinia/PiniaPluginExtension.ts
+++ src/Sushi.MediaKiwi.Vue/src/models/pinia/PiniaPluginExtension.ts
@@ -1,17 +1,16 @@
-import 'pinia'
-import type { Ref } from 'vue'
-import type { Router } from 'vue-router'
-import type { MediaKiwiState } from "../../stores/index"
+import "pinia";
+import type { Ref } from "vue";
+import type { Router } from "vue-router";
+import type { MediaKiwiState } from "../../stores/index";
 
-declare module 'pinia' {
-  export interface PiniaCustomProperties  {
+declare module "pinia" {
+  export interface PiniaCustomProperties {
     // by using a setter we can allow both strings and refs
-    set hello(value: string | Ref<string>)
-    get hello(): string
-
+    set hello(value: string | Ref<string>);
+    get hello(): string;
 
     // type the router added by the plugin above (#adding-new-external-properties)
-    router: Router
-    mediakiwi: MediaKiwiState
+    router: Router;
+    mediakiwi: MediaKiwiState;
   }
 }
