f2cb7e817fd88e5afd7cee404023f1b5f12b97a4
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-08-21 12:51:07 +0200

    Icon errors

--- src/Sushi.MediaKiwi.Vue/src/plugins/icons/icons.ts
+++ src/Sushi.MediaKiwi.Vue/src/plugins/icons/icons.ts
@@ -1,6 +1,6 @@
 // remember to add the icons to the "../models/enum/Icons.ts" file so they are typesafe in the app an can be used whithin the MKVue project
 import { IconOptions } from "vuetify/lib/framework.mjs";
-import { aliases, mdi } from "vuetify/lib/iconsets/mdi-svg";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import { mediakiwiIconAliases } from "./IconAliases";
 
 // custom Icon options for mdi svgs
