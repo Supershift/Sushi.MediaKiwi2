5781b23c6620489fbd4dd5c2e49d95d3c41ab309
Luigi Arends <luigi.arends@supershift.nl>
2023-09-18 16:35:43 +0200

    added some stuff for drawer

--- src/Sushi.MediaKiwi.Vue/src/components/MkNavigation/__tests__/MkNavigationDrawer.test.ts
+++ src/Sushi.MediaKiwi.Vue/src/components/MkNavigation/__tests__/MkNavigationDrawer.test.ts
@@ -3,7 +3,7 @@ import "reflect-metadata";
 import MkNavigationDrawer from "../MkNavigationDrawer.vue";
 import { describe, expect, vi, beforeEach } from "vitest";
 import { container } from "tsyringe";
-import { mountAsync } from "@test/utils/mount";
+import { mountAsync, mountWithLayoutAsync } from "@test/utils/mount";
 import { NavigationItem } from "@/models";
 import { computed } from "vue";
 
@@ -11,7 +11,39 @@ import { computed } from "vue";
 // vi.mock("i18next");
 vi.mock("@azure/msal-browser");
 vi.mock("@/stores/useMediakiwiStore");
-vi.mock("@/composables/useNavigation");
+vi.mock("@/composables/useNavigation", () => {
+    return {
+        useNavigation: () => {
+            return {
+                navigateTo: () => {
+                    return Promise.resolve();
+                },
+                determineIfSectionIsActive: () => {
+                    return true;
+                },
+                getAllItemsBasedOnSection: () => {
+                    return [];
+                },
+                getItemsBasedOnRoot: () => {
+                    return [];
+                },
+                getItemsBasedOnSection: () => {
+                    return [];
+                },
+                currentRootItem: computed(() => {
+                    return {
+                        id: 1,
+                        name: "Section One",
+                        sortOrder: 0,
+                        icon: "$arrowLeft",
+                    };
+                }),
+            };
+        },
+        
+    };
+});
+
 vi.mock("@/composables/useI18next", () => {
     return {
         defaultT: (key: string) => computed(() => key),
@@ -36,6 +68,17 @@ describe("MkNavigationDrawer", () => {
     beforeEach(() => {
       container.reset();
       vi.clearAllMocks();
+      global.ResizeObserver = class ResizeObserver {
+        observe() {
+          // do nothing
+        }
+        unobserve() {
+          // do nothing
+        }
+        disconnect() {
+          // do nothing
+        }
+      };
     });
     afterEach(() => {
         vi.resetAllMocks();
@@ -45,15 +88,14 @@ describe("MkNavigationDrawer", () => {
         // TODO: Mock the i18next
 
         // Mount the component in a suspense wrapper
-        const wrapper = await mountAsync(MkNavigationDrawer, {
+        const wrapper = await mountWithLayoutAsync(MkNavigationDrawer, 
             props,
-        });
+        );
     
         // Check that the component is rendered and contains an icon 
         const outputHtml = wrapper.html();
         expect(outputHtml).toBeTruthy(); // Everything in JavaScript is truthy, except false, 0, '', null, undefined, and NaN.
-        // expect(outputHtml).toContain("v-navigation-drawer");
-        console.log(wrapper.vm.$data);
+        expect(outputHtml).toContain("v-navigation-drawer");
     });
     
   });
