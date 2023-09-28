3f06de0c93de104b0ecdfefe1e1aacb69a00f1d8
Luigi Arends <luigi.arends@gmail.com>
2023-09-28 09:43:54 +0200

    feature/shif-365-implement-textfield component with design guidelines (#173)
    * added global settings and changed some css
    
    * added outline as default
    
    * fixed outline issue discovered on other branch
    
    * color fixes for disabled

--- src/Sushi.MediaKiwi.Vue/src/plugins/vuetify/GlobalConfiguration.ts
+++ src/Sushi.MediaKiwi.Vue/src/plugins/vuetify/GlobalConfiguration.ts
@@ -80,6 +80,8 @@ export const globalConfiguration: VuetifyOptions = {
     },
     VTextField: {
       variant: "outlined",
+      color: "outline",
+      class: "v-field--mediakiwi",
     },
     VSelect: {
       variant: "outlined",
