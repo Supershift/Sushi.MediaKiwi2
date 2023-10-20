0803d85b84f6b2a686bc63a424f77e0942212e2a
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-10-20 16:14:33 +0200

    Implemented method to set the browserr's page title

--- src/Sushi.MediaKiwi.Vue/src/helpers/registerRouter.ts
+++ src/Sushi.MediaKiwi.Vue/src/helpers/registerRouter.ts
@@ -1,9 +1,14 @@
+import { MediakiwiVueOptions } from "@/models";
+import { setPageTitle } from "@/router/setPageTitle";
 import { DependencyContainer } from "tsyringe";
 import { Router } from "vue-router";
 
-export function registerRouter(container: DependencyContainer, router: Router) {
+export function registerRouter(container: DependencyContainer, router: Router, options?: MediakiwiVueOptions) {
   // register router
   container.register("Router", {
     useValue: router,
   });
+
+  // Add method to set page title
+  setPageTitle(router, options);
 }
---
+++ src/Sushi.MediaKiwi.Vue/src/router/setPageTitle.ts
@@ -0,0 +1,50 @@
+import { MediakiwiVueOptions, NavigationItem } from "@/models";
+import { useMediakiwiStore } from "@/stores";
+import { type RouteLocationNormalized, type Router } from "vue-router";
+
+/**
+ * Add a beforeEach method to set the page title
+ * Based on the MediaKiwi Vue Options Title, Section and (Parent)Page Name
+ *
+ *
+ */
+export function setPageTitle(router: Router, options?: MediakiwiVueOptions): void {
+  // Get the provided title from the options
+  document.title = <string>options?.title;
+
+  router.beforeEach((to: RouteLocationNormalized) => {
+    // Set the Base Title
+    // Example: MediaKiwi 2.0
+    let pageTitle = <string>options?.title;
+
+    if (to.meta?.navigationItem) {
+      // Get the navigation item from the meta of the route
+      const navigationItem = <NavigationItem>to.meta?.navigationItem;
+      console.log(navigationItem);
+
+      if (navigationItem) {
+        // Get the sections from the store
+        const { sections, getParentName } = useMediakiwiStore();
+
+        // Find the matching section
+        const section = sections?.find((x) => x.id === navigationItem.sectionId);
+
+        // Prepend the section name to the title
+        // Example: Admin - MediaKiwi 2.0
+        if (section?.name) {
+          pageTitle = `${section.name} - ${pageTitle}`;
+        }
+
+        // Prepend the navigation item name to the title
+        // Example: Views - Admin - MediaKiwi 2.0
+        const parentName = getParentName(navigationItem);
+        if (parentName) {
+          pageTitle = `${parentName} - ${pageTitle}`;
+        }
+      }
+    }
+
+    // Set the document title
+    document.title = pageTitle;
+  });
+}
