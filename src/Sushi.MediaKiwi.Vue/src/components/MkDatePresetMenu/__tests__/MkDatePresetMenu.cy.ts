d4b2803196e59a906ca1d79079a8d87fc4b3c720
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-11-23 14:05:19 +0100

    Delete MkDatePresetMenu.cy.ts

--- src/Sushi.MediaKiwi.Vue/src/components/MkDatePresetMenu/__tests__/MkDatePresetMenu.cy.ts
+++
@@ -1,8 +0,0 @@
-import MkDatePresetMenu from "../MkDatePresetMenu.vue";
-
-describe("<MkDatePresetMenu />", () => {
-  it("renders", () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(MkDatePresetMenu);
-  });
-});
