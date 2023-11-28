5e5751e5314f4ea38c5a63f4c9523349cae7f849
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2023-11-28 11:17:55 +0100

    Update VuetifyOptions.ts

--- src/Sushi.MediaKiwi.Vue/src/plugins/vuetify/VuetifyOptions.ts
+++ src/Sushi.MediaKiwi.Vue/src/plugins/vuetify/VuetifyOptions.ts
@@ -3,9 +3,6 @@ import type { VuetifyOptions } from "vuetify";
 import * as VComponents from "vuetify/components";
 import * as VDirectives from "vuetify/directives";
 
-// Still in labs, but we need it
-import { VStepper, VDatePicker } from "vuetify/lib/labs/components.mjs";
-
 import { md3 } from "vuetify/blueprints";
 
 import { MediakiwiIconOptions } from "@/plugins/icons/icons";
@@ -19,11 +16,9 @@ const defaultVuetifyOptions = <VuetifyOptions>{
   blueprint: md3,
   components: {
     ...VComponents,
-    VStepper,
-    VDatePicker,
   },
   directives: VDirectives,
   icons: MediakiwiIconOptions,
 };
 
-export { defaultVuetifyOptions };
+export default defaultVuetifyOptions;
