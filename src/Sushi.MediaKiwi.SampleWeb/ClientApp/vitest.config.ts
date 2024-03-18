f92fca57f9b992e8e9f95633435eb23e46054851
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2024-03-18 15:41:30 +0100

    Update vitest.config.ts

--- src/Sushi.MediaKiwi.SampleWeb/ClientApp/vitest.config.ts
+++ src/Sushi.MediaKiwi.SampleWeb/ClientApp/vitest.config.ts
@@ -19,6 +19,7 @@ export default mergeConfig(
       outputFile: {
         junit: "test/unit/coverage/TEST-junit.xml",
       },
+      exclude: ["node_modules"],
       coverage: {
         provider: "istanbul",
         reporter: ["text", "cobertura", "json", ["html", { subdir: "html" }]],
