6b6419d015ca372c4681020ec91b4fe3b701f73d
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2024-04-03 17:34:27 +0200

    Added Basic UI to Add and Edit Navigation Items

--- src/Sushi.MediaKiwi.Vue/src/services/index.ts
+++ src/Sushi.MediaKiwi.Vue/src/services/index.ts
@@ -1,4 +1,4 @@
-export { type INavigationConnector } from "@/services/INavigationConnector";
+export type { INavigationConnector } from "@/services/interfaces";
 export { type IViewConnector } from "@/services/IViewConnector";
 export { type ISectionConnector } from "@/services/ISectionConnector";
 export { type ILocaleConnector } from "@/services/ILocaleConnector";
--- src/Sushi.MediaKiwi.Vue/src/views/modules.ts
+++ src/Sushi.MediaKiwi.Vue/src/views/modules.ts
@@ -6,6 +6,9 @@ import SectionEdit from "./SectionEdit.vue";
 import LocaleOverview from "./LocaleOverview.vue";
 import LocaleEdit from "./LocaleEdit.vue";
 import StyleGuideView from "./StyleGuideView.vue";
+import NavigationItemOverview from "./NavigationItemOverview.vue";
+import NavigationItemEdit from "./NavigationItemEdit.vue";
+
 const modules: Record<string, RouteComponent> = {
   MkViewOverview: ViewOverview,
   MkViewEdit: ViewEdit,
@@ -14,6 +17,8 @@ const modules: Record<string, RouteComponent> = {
   MkLocaleOverview: LocaleOverview,
   MkLocaleEdit: LocaleEdit,
   MkStyleGuideView: StyleGuideView,
+  MkNavigationItemOverview: NavigationItemOverview,
+  MkNavigationItemEdit: NavigationItemEdit,
 };
 
 export { modules };
