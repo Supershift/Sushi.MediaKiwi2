28b615224206b5139fd2b826c189cded572dcd8b
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2024-04-16 13:30:35 +0200

    Removed Examples.

--- src/Sushi.MediaKiwi.Vue/sample/main.ts
+++ src/Sushi.MediaKiwi.Vue/sample/main.ts
@@ -48,9 +48,6 @@ const mediakiwiOptions = <MediakiwiVueOptions>{
   identity: {
     scopes: [`api://${import.meta.env.VITE_APP_MEDIAKIWI_MSALCONFIG_AUTH_CLIENTID}/access_via_approle_assignments`],
   },
-  emptyState: {
-    image: "/M-C_Mascot_illo_NoSearchResults_AW__Transparent BG.svg",
-  },
 };
 
 // Create the app
