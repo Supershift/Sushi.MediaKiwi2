f28ea51ebf43a7b13cbe9128be19df1a390bc943
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2024-04-23 14:00:01 +0200

    Create CountriesEmpty.vue

---
+++ src/Sushi.MediaKiwi.Vue/sample/views/CountriesEmpty.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+  import { Country } from "./../models/Country";
+  import { CountryConnector } from "./../services/CountryConnector";
+  import { MkTable } from "@/components";
+  import { useI18next, useKeyboardShortcuts } from "@/composables";
+  import { ListResult, TableMap, Paging, KeyboardShortcutCollection } from "@/models";
+  import { container } from "tsyringe";
+  import { reactive } from "vue";
+  import { onDeactivated } from "vue";
+  import { ref } from "vue";
+  import CountryEdit from "./CountryEdit.vue";
+
+  // inject dependencies
+  const { t } = await useI18next();
+
+  // define reactive variables
+  const currentPagination = ref<Paging>({});
+  const state = reactive({
+    countries: <ListResult<Country>>{},
+    addCountry: false,
+  });
+
+  /** Define Keybinding collection */
+  const shortCuts: KeyboardShortcutCollection = {
+    "shift+c": (e: KeyboardEvent) => {
+      e.preventDefault();
+      alert("You've pressed shift+c, this is a little secret!");
+    },
+  };
+
+  // load data
+  async function LoadData() {
+    state.countries = {
+      totalCount: 0,
+      result: [],
+    };
+  }
+
+  function openDialog() {
+    state.addCountry = true;
+  }
+</script>
+<template>
+  <mk-table
+    v-model:currentPagination="currentPagination"
+    :api-result="state.countries"
+    :data="state.countries?.result"
+    @load="LoadData"
+    :item-id="(item: Country) => item.code"
+    item-view-id="CountryEdit"
+    new
+    :new-title="t('Add Country').toString()"
+    new-emit
+    @click:new="openDialog"
+  >
+    <template #thead>
+      <th>Code</th>
+      <th>Name</th>
+    </template>
+
+    <template #tbody="dataItem: Country">
+      <td>{{ dataItem.code }}</td>
+      <td>{{ dataItem.name }}</td>
+    </template>
+  </mk-table>
+
+  <CountryEdit v-model="state.addCountry" />
+</template>
