db04b8c42af498b4f8146e93a3e9cc786c71e18d
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2024-04-23 13:32:52 +0200

    Added disabled and tooltip to the Section

--- src/Sushi.MediaKiwi.Vue/src/models/api/Section.ts
+++ src/Sushi.MediaKiwi.Vue/src/models/api/Section.ts
@@ -5,4 +5,8 @@ export interface Section {
   icon?: string;
   /** If not empty, access to this screen is restricted to these roles. */
   roles?: string[];
+  /** Sets the section in a disabled state */
+  disabled: boolean;
+  /** Tooltip text */
+  tooltip?: string;
 }
