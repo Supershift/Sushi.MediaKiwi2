4666f903ded3c39c79f7b52d41feb76fb866a008
Mark de Vries <mark.devries@supershift.nl>
2024-05-15 11:43:00 +0200

    reworked useValidationRules

--- src/Sushi.MediaKiwi.Vue/src/composables/__tests__/useValidationRules.spec.ts
+++ src/Sushi.MediaKiwi.Vue/src/composables/__tests__/useValidationRules.spec.ts
@@ -2,35 +2,53 @@ import { describe, it, expect } from "vitest";
 import { useValidationRules } from "../useValidationRules";
 
 describe("useValidationRules", () => {
-  const { required, min, max, email, numeric } = useValidationRules();
+  const { required, minLength, maxLength, email, numeric, alphaNumericNoSpace, alphaNumericWithSpace } = useValidationRules();
 
   it("should validate that a value is required", () => {
     const message = "This field is required";
-    expect(required("", message)[0]()).toBe(message);
-    expect(required("Some value", message)[0]()).toBe(true);
+    expect(required("", message)).toBe(message);
+    expect(required("Some value", message)).toBe(true);
   });
 
   it("should validate the minimum value length", () => {
     const message = "Minimum length is 5";
-    expect(min("abc", 5, message)[0]()).toBe(message);
-    expect(min("abcdef", 5, message)[0]()).toBe(true);
+    expect(minLength("abc", 5, message)).toBe(message);
+    expect(minLength("abcdef", 5, message)).toBe(true);
   });
 
   it("should validate the maximum value length", () => {
     const message = "Maximum length is 10";
-    expect(max("abcdefghijk", 10, message)[0]()).toBe(message);
-    expect(max("abcde", 10, message)[0]()).toBe(true);
+    expect(maxLength("abcdefghijk", 10, message)).toBe(message);
+    expect(maxLength("abcde", 10, message)).toBe(true);
   });
 
   it("should validate that a value is a valid email address", () => {
     const message = "Invalid email address";
-    expect(email("notanemail", message)[0]()).toBe(message);
-    expect(email("test@example.com", message)[0]()).toBe(true);
+    expect(email("notanemail", message)).toBe(message);
+    expect(email("test@example.com", message)).toBe(true);
   });
 
   it("should validate that a value is numeric", () => {
     const message = "Value must be numeric";
-    expect(numeric("123abc", message)[0]()).toBe(message);
-    expect(numeric("123", message)[0]()).toBe(true);
+    expect(numeric("123abc", message)).toBe(message);
+    expect(numeric("123", message)).toBe(true);
+  });
+
+  it("Should only allow alpha-numeric characters", () => {
+    const message = "Only alpha-numeric characters are allowed";
+    expect(alphaNumericNoSpace("abc", message)).toBe(true);
+    expect(alphaNumericNoSpace("123", message)).toBe(true);
+    expect(alphaNumericNoSpace("123abc", message)).toBe(true);
+    expect(alphaNumericNoSpace("123 abc", message)).toBe(message);
+    expect(alphaNumericNoSpace("abc@", message)).toBe(message);
+  });
+
+  it("Should only allow alpha-numeric characters and whitespace", () => {
+    const message = "Only alpha-numeric characters are allowed";
+    expect(alphaNumericWithSpace("abc", message)).toBe(true);
+    expect(alphaNumericWithSpace("123", message)).toBe(true);
+    expect(alphaNumericWithSpace("123abc", message)).toBe(true);
+    expect(alphaNumericWithSpace("123 abc", message)).toBe(true);
+    expect(alphaNumericWithSpace("abc@", message)).toBe(message);
   });
 });
--- src/Sushi.MediaKiwi.Vue/src/composables/useValidationRules.ts
+++ src/Sushi.MediaKiwi.Vue/src/composables/useValidationRules.ts
@@ -1,27 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /** Preset Validation Rules to be used for the ValidationRule[] for form inputs
  *  @param {string} value - The value of the input field
- *  @param {string} message - The message to be displayed if the validation fails
+ *  @param {string} message - Optioanl, the message to be displayed if the validation fails. Otherwiste a default message is shown.
  *  @example const rules: ValidationRule[] = useValidationRules().required('', 'This field is required');
  *  @returns {object} { required, min, max, email, numeric }
  */
 export const useValidationRules = () => {
   /** value is required */
-  const required = (value: any, message: string) => [() => !!value || message];
-  /** minimal value length required*/
-  const min = (value: string, min: number, message: string) => [() => value.length >= min || message];
+  const required = (value: any, message?: string) => !!value || (message ?? "This value is required");
+  /** minimum value length required*/
+  const minLength = (value: string, min: number, message?: string) => value.length >= min || (message ?? `Value must be at least ${maxLength} characters`);
   /** maximum value length allowed */
-  const max = (value: string, max: number, message: string) => [() => value.length <= max || message];
+  const maxLength = (value: string, max: number, message?: string) => value.length <= max || (message ?? `Value must not be longer than ${max} characters`);
   /** value must be a valid email address */
-  const email = (value: string, message: string) => [() => /.+@.+\..+/.test(value) || message];
+  const email = (value: string, message?: string) => /.+@.+\..+/.test(value) || (message ?? "This is not a valid e-mail adress");
   /** value must be numeric */
-  const numeric = (value: string, message: string) => [() => /^\d+$/.test(value) || message];
-  /** minimal value of number */
-  const minValue = (value: number, min: number, message: string) => [() => value >= min || message];
+  const numeric = (value: string, message?: string) => /^\d+$/.test(value) || (message ?? "Only numbers are allowed");
+  /** minimum value of number */
+  const minValue = (value: number, min: number, message?: string) => value >= min || (message ?? `Only values greater than or equal to ${min} are allowed`);
   /** value must be alphanumeric character, whitespace allowed */
-  const alphaNumericWithSpace = (value: string, message: string) => [() => /^[a-zA-Z0-9_\s]*$/.test(value) || message];
+  const alphaNumericWithSpace = (value: string, message?: string) =>
+    /^[a-zA-Z0-9_\s]*$/.test(value) || (message ?? "Only alpha-numeric characters and space are allowed");
   /** value must be alphanumeric character, no whitespace allowed */
   const alphaNumericNoSpace = (value: string, message?: string) => /^\w+$/.test(value) || (message ?? "Only alpha-numeric characters are allowed");
 
-  return { required, min, max, email, numeric, minValue, alphaNumericWithSpace, alphaNumericNoSpace };
+  return { required, minLength, maxLength, email, numeric, minValue, alphaNumericWithSpace, alphaNumericNoSpace };
 };
