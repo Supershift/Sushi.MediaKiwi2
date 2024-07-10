053eebb15168a4ad5b95498bf99646cf28fa0922
Luigi Arends <luigi.arends@supershift.nl>
2024-07-10 13:50:02 +0200

    added models for display options

--- src/Sushi.MediaKiwi.Vue/src/models/table/index.ts
+++ src/Sushi.MediaKiwi.Vue/src/models/table/index.ts
@@ -1,6 +1,7 @@
 export type { TableFilter } from "./TableFilter";
 export type { TableFilterItem } from "./TableFilterItem";
 export type { TableFilterValue } from "./TableFilterValue";
+export type { TableColumn } from "./TableColumn";
 export type { TableMap } from "./TableMap";
 export type { TableMapItem } from "./TableMapItem";
 export type { TableSortingOptions } from "./TableSortingOptions";
