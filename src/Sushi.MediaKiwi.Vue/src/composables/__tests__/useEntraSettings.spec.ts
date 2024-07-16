c2f13d884dea48422858e63a07df23ba31da9f08
Luigi Arends <luigi.arends@supershift.nl>
2024-07-16 13:11:08 +0200

    added test to composable

---
+++ src/Sushi.MediaKiwi.Vue/src/composables/__tests__/useEntraSettings.spec.ts
@@ -0,0 +1,45 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { useEntraSettings } from '../useEntraSettings';
+import { MediakiwiVueOptions } from '@/models';
+
+vi.mock('axios');
+// Mock the external modules
+vi.mock('@/services/axios/createAxiosClient', () => ({
+  createPublicAxiosClient: vi.fn(),
+}));
+vi.mock('@/services/IdentityProviderConnector', () => {
+  return {
+    IdentityProviderConnector: vi.fn().mockImplementation(() => {
+      return {
+        GetEntraSettings: vi.fn().mockResolvedValue({
+          instance: 'https://login.microsoftonline.com/',
+          tenantId: 'tenant-id',
+          clientId: 'client-id',
+        }),
+      };
+    }),
+  };
+});
+
+describe('useEntraSettings', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('getEntraSettings returns the correct settings', async () => {
+    const { getEntraSettings } = useEntraSettings();
+
+    const settings = await getEntraSettings();
+
+    expect(settings.authority).toBe('https://login.microsoftonline.com/tenant-id');
+  });
+
+  it('fillEntraSettings fills the options correctly', async () => {
+    const { fillEntraSettings } = useEntraSettings();
+    const options = await fillEntraSettings({ apiBaseUrl: "" } as MediakiwiVueOptions);
+
+    expect(options.msalConfig.auth.clientId).toBe("client-id");
+    expect(options.msalConfig.auth.authority).toContain('https://login.microsoftonline.com');
+    expect(options.identity.scopes).toEqual(['api://client-id/access_via_approle_assignments']);
+  });
+});
