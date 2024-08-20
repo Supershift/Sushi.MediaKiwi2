cf42f5e3d4c6ddeab763f52d8da2cdd15fc50708
Mark de Vries <mark.devries@supershift.nl>
2024-08-20 15:50:50 +0200

    use lower camelcase on continuation values

--- src/Sushi.MediaKiwi.Services/ContinuationValues.cs
+++ src/Sushi.MediaKiwi.Services/ContinuationValues.cs
@@ -1,9 +1,11 @@
-﻿namespace Sushi.MediaKiwi.Services
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sushi.MediaKiwi.Services
 {
     /// <summary>
     /// Represents values used when adding continuous scroll on datasets.
     /// </summary>
-    public record ContinuationValues(string? Token = null, int MaxItems = 10)
+    public record ContinuationValues([property: FromQuery(Name = "token")] string? Token = null, [property: FromQuery(Name = "maxItems")] int MaxItems = 10)
     {
         /// <summary>
         /// Gets a <see cref="ContinuationValues"/> instance with default values (maxItems = 10, token = NULL).
