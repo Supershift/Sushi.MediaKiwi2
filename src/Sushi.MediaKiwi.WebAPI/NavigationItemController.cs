4e8b38bd6c1eceafb0ae7096d6e615d0ec26e7d4
Mark de Vries <mark.devries@supershift.nl>
2024-08-21 09:44:33 +0200

    fixed unsupported media type error in GET nav items

--- src/Sushi.MediaKiwi.WebAPI/NavigationItemController.cs
+++ src/Sushi.MediaKiwi.WebAPI/NavigationItemController.cs
@@ -39,15 +39,14 @@ namespace Sushi.MediaKiwi.WebAPI
 
         /// <summary>
         /// Gets all navigation items for the given filters.
-        /// </summary>
-        /// <param name="query"></param>
+        /// </summary>        
         /// <returns></returns>
         [HttpGet]
         [QueryStringSorting<NavigationItemsSortMap>()]
-        public async Task<ActionResult<ListResult<NavigationItem>>> GetNavigationItems(GetNavigationItemsQuery query)
+        public async Task<ActionResult<ListResult<NavigationItem>>> GetNavigationItems(string? sectionId, [FromQuery] PagingValues pagingValues)
         {
             var sortValues = _sortingRetriever.GetSorting<NavigationItem>();
-            var result = await _navigationItemService.GetAllAsync(query.sectionID, query.Page, sortValues);
+            var result = await _navigationItemService.GetAllAsync(sectionId, pagingValues, sortValues);
             return this.CreateResponse(result);
         }
         
@@ -62,22 +61,6 @@ namespace Sushi.MediaKiwi.WebAPI
         {
             var result = await _navigationItemService.GetAsync(id);
             return this.CreateResponse(result);
-        }
-
-        /// <summary>
-        /// Query for GetNavigationItems
-        /// </summary>
-        public class GetNavigationItemsQuery
-        {
-            /// <summary>
-            /// Paging values.
-            /// </summary>
-            public PagingValues Page { get; set; } = null!;
-
-            /// <summary>
-            /// If set to true, only locales with enabled set to true are returned.
-            /// </summary>
-            public string? sectionID { get; set; }
         }
     }
 }
