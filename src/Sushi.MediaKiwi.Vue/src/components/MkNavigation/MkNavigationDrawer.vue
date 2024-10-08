8a6c3d02ca9f571478516bbaaf3dc14b222a2d30
Dennis Geldhof <dennis.geldhof@supershift.nl>
2024-10-08 11:33:03 +0200

    Allow back button navigation to be overwritten through the store

--- src/Sushi.MediaKiwi.Vue/src/components/MkNavigation/MkNavigationDrawer.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkNavigation/MkNavigationDrawer.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
+  import { computed } from "vue";
   import MkNavigationItem from "@/components/MkNavigation/MkNavigationItem.vue";
+  import { useMediakiwiStore } from "@/stores";
   import { useNavigation } from "@/composables/useNavigation";
   import { useI18next } from "@/composables/useI18next";
   import { IconsLibrary } from "@/models";
+  import { useRouter } from "@/router";
 
-  // define properties
   defineEmits(["change"]);
 
-  // inject dependencies
   const { getItemsBasedOnRoot, navigateTo, currentRootItem } = useNavigation(); // also calls store within this composable
   const { defaultT } = await useI18next();
+  const router = useRouter();
+  const store = useMediakiwiStore();
+
+  const navigate = computed(() => (item: any) => {
+    if (store.navigationBackUrlOverwrite) {
+      const overwrite = store.navigationBackUrlOverwrite;
+      store.navigationBackUrlOverwrite = undefined;
+
+      router.push(overwrite);
+    } else {
+      navigateTo(item);
+    }
+  });
 </script>
 <template>
   <v-navigation-drawer class="pa-3">
@@ -21,7 +35,7 @@
         rounded="pill"
         class="mb-2"
         :prepend-icon="IconsLibrary.arrowLeft"
-        @click.stop="navigateTo(currentRootItem)"
+        @click.stop="navigate(currentRootItem)"
       />
 
       <div id="navigationDrawerInfo" class="mb-4"></div>
