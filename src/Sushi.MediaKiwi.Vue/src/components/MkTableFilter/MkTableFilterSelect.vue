67027dca337011c297ee421e8a9ac41f89ab49e9
Dennis Geldhof <dennis.geldhof@supershift.nl>
2024-10-09 10:22:26 +0200

    Fix formatting

--- src/Sushi.MediaKiwi.Vue/src/components/MkTableFilter/MkTableFilterSelect.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkTableFilter/MkTableFilterSelect.vue
@@ -1,41 +1,47 @@
 <script setup lang="ts">
-import { useI18next } from "@/composables/useI18next";
-import type { TableFilterItem, TableFilterValue } from "@/models/table";
-import { computed, ref } from "vue";
-import MkTableFilterDialog from "./MkTableFilterDialog.vue";
+  import { useI18next } from "@/composables/useI18next";
+  import type { TableFilterItem, TableFilterValue } from "@/models/table";
+  import { computed, ref } from "vue";
+  import MkTableFilterDialog from "./MkTableFilterDialog.vue";
 
-// inject dependencies
-const { defaultT } = await useI18next("MkFilter");
+  // inject dependencies
+  const { defaultT } = await useI18next("MkFilter");
 
-const props = defineProps<{
-  tableFilterItem: TableFilterItem;
-}>();
+  const props = defineProps<{
+    tableFilterItem: TableFilterItem;
+  }>();
 
-const modelValue = defineModel<TableFilterValue>({ required: true });
+  const modelValue = defineModel<TableFilterValue>({ required: true });
 
-const emit = defineEmits<{
-  (e: "click:close"): void;
-}>();
+  const emit = defineEmits<{
+    (e: "click:close"): void;
+  }>();
 
-// Create proxy model to prevent direct mutation
-const model = ref(modelValue.value);
+  // Create proxy model to prevent direct mutation
+  const model = ref(modelValue.value);
 
-// Additional rules for the input field
-const additionalRules = computed(() => props.tableFilterItem.rules || []);
+  // Additional rules for the input field
+  const additionalRules = computed(() => props.tableFilterItem.rules || []);
 
-function applyFilter() {
-  if (model.value) {
-    modelValue.value = model.value;
+  function applyFilter() {
+    if (model.value) {
+      modelValue.value = model.value;
+    }
   }
-}
 </script>
 
 <template>
   <MkTableFilterDialog :table-filter-item="tableFilterItem" @close="emit('click:close')" @apply="applyFilter">
     <div class="pa-6">
-      <v-autocomplete v-model="model" hide-details="auto" :items="tableFilterItem.options"
-        :label="tableFilterItem.inputLabel || defaultT('Value')" return-object :rules="[...additionalRules]"
-        autofocus></v-autocomplete>
+      <v-autocomplete
+        v-model="model"
+        hide-details="auto"
+        :items="tableFilterItem.options"
+        :label="tableFilterItem.inputLabel || defaultT('Value')"
+        return-object
+        :rules="[...additionalRules]"
+        autofocus
+      ></v-autocomplete>
     </div>
   </MkTableFilterDialog>
 </template>
--- src/Sushi.MediaKiwi.Vue/src/components/MkTableFilter/MkTableFilterTextField.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkTableFilter/MkTableFilterTextField.vue
@@ -1,51 +1,50 @@
 <script setup lang="ts">
-import { useI18next } from "@/composables/useI18next";
-import type { TableFilterItem, TableFilterValue } from "@/models/table";
-import { computed, ref } from "vue";
-import MkTableFilterDialog from "./MkTableFilterDialog.vue";
-import { TableFilterType } from "@/models";
+  import { useI18next } from "@/composables/useI18next";
+  import type { TableFilterItem, TableFilterValue } from "@/models/table";
+  import { computed, ref } from "vue";
+  import MkTableFilterDialog from "./MkTableFilterDialog.vue";
+  import { TableFilterType } from "@/models";
 
-const { t, defaultT } = await useI18next("MkFilter");
+  const { t, defaultT } = await useI18next("MkFilter");
 
-// inject dependencies
-const props = defineProps<{
-  tableFilterItem: TableFilterItem;
-}>();
+  // inject dependencies
+  const props = defineProps<{
+    tableFilterItem: TableFilterItem;
+  }>();
 
-const modelValue = defineModel<TableFilterValue>({ required: true });
+  const modelValue = defineModel<TableFilterValue>({ required: true });
 
-// Create proxy model to prevent direct mutation
-const model = ref(modelValue.value?.value || "");
+  // Create proxy model to prevent direct mutation
+  const model = ref(modelValue.value?.value || "");
 
-const emit = defineEmits<{
-  (e: "click:close"): void;
-}>();
+  const emit = defineEmits<{
+    (e: "click:close"): void;
+  }>();
 
-const inputLabel = computed(() => {
-  if (props.tableFilterItem.inputLabel) {
-    return props.tableFilterItem.inputLabel;
+  const inputLabel = computed(() => {
+    if (props.tableFilterItem.inputLabel) {
+      return props.tableFilterItem.inputLabel;
+    }
+    return props.tableFilterItem.type === TableFilterType.Contains ? defaultT.value("Contains") : defaultT.value("Value");
+  });
+
+  // Additional rules for the input field
+  const additionalRules = computed(() => props.tableFilterItem.rules || []);
+
+  function applyFilter() {
+    if (model.value) {
+      modelValue.value = {
+        value: model.value,
+        title: model.value,
+      };
+    }
   }
-  return props.tableFilterItem.type === TableFilterType.Contains ? defaultT.value("Contains") : defaultT.value("Value");
-});
-
-// Additional rules for the input field
-const additionalRules = computed(() => props.tableFilterItem.rules || []);
-
-function applyFilter() {
-  if (model.value) {
-    modelValue.value = {
-      value: model.value,
-      title: model.value,
-    };
-  }
-}
 </script>
 
 <template>
   <MkTableFilterDialog :table-filter-item="tableFilterItem" @close="emit('click:close')" @apply="applyFilter">
     <div class="pa-6">
-      <v-text-field v-model="model" :label="inputLabel" hide-details="auto" :rules="[...additionalRules]" autofocus>
-      </v-text-field>
+      <v-text-field v-model="model" :label="inputLabel" hide-details="auto" :rules="[...additionalRules]" autofocus> </v-text-field>
     </div>
   </MkTableFilterDialog>
 </template>
