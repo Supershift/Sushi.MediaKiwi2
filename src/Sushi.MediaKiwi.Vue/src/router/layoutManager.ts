ccc6f5df5ffe511e8ad0200eae82fb625c0c32c8
Luigi Arends <luigi.arends@supershift.nl>
2024-10-16 16:44:37 +0200

    implement layoutmanager and add layout to nav provider via builder

---
+++ src/Sushi.MediaKiwi.Vue/src/router/layoutManager.ts
@@ -0,0 +1,29 @@
+import { Router } from "vue-router"
+
+/**
+ * This Manager is used to dynamically update the Layouts system.
+ * When the layout we want to display is not found, it loads the default layout App Layout Default.vue
+ **/
+export async function loadLayout(router: Router) {
+  if (router) {
+    router.afterEach(async (to, _from) => {
+      if (to && to.meta && to.meta.layout) {
+        try {
+          console.log('Init Layout Manager...');
+          let layout = to.meta.layout;
+          let layoutComponent = await import(`@/layouts/${layout}.vue`);
+          to.meta.layoutComponent = layoutComponent.default;
+          console.log(`Found layout ${layout}`);
+        } catch (e) {
+          console.log('No layout found, use default instead');
+          let layout = 'DefaultLayout';
+          let layoutComponent = await import(`@/layouts/${layout}.vue`);
+          to.meta.layoutComponent = layoutComponent.default;
+        }
+      } else {
+        console.log('Loaded without meta layout')
+      }
+    });
+  }
+
+}
