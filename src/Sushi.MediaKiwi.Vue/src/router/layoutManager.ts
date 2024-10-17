9b91046977073630f47f707e84ef1ad70cdd947a
Luigi Arends <luigi.arends@supershift.nl>
2024-10-17 11:27:59 +0200

    removed comments and added some consts

--- src/Sushi.MediaKiwi.Vue/src/router/layoutManager.ts
+++ src/Sushi.MediaKiwi.Vue/src/router/layoutManager.ts
@@ -2,26 +2,23 @@ import { Router } from "vue-router"
 import { MkLayout } from "@/constants";
 /**
  * This Manager is used to dynamically update the Layouts system.
- * When the layout we want to display is not found, it loads the default layout App Layout Default.vue
+ * When the layout we want to display is not found, it loads the default layout MkLayout.Default, which is @/layouts/DefaultLayout.vue
+ * MK ships with DefaultLayout.vue and FullScreenLayout.vue (MkLayout.Full)
  **/
-export async function loadLayout(router: Router) {
+export async function layoutManager(router: Router) {
   if (router) {
     router.afterEach(async (to, _from) => {
       if (to && to.meta && to.meta.layout) {
         try {
-          console.log('Init Layout Manager...');
-          let layout = to.meta.layout;
-          let layoutComponent = await import(`@/layouts/${layout}.vue`);
+          const assignedLayout = to.meta.layout || MkLayout.Default;
+          const layoutComponent = await import(`@/layouts/${assignedLayout}.vue`);
           to.meta.layoutComponent = layoutComponent.default;
-          console.log(`Found layout ${layout}`);
         } catch (e) {
-          console.log('No layout found, use default instead');
-          let layout = MkLayout.Default;
-          let layoutComponent = await import(`@/layouts/${layout}.vue`);
+          const layoutComponent = await import(`@/layouts/${MkLayout.Default}.vue`);
           to.meta.layoutComponent = layoutComponent.default;
         }
       } else {
-        console.log('Loaded without meta layout')
+        console.log('Loaded without layout')
       }
     });
   }
