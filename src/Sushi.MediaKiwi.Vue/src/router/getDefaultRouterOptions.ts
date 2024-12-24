2fa5949d46d0d37e9e7f76b344225114bd7c138d
Luigi Arends <luigi.arends@supershift.nl>
2024-12-24 10:58:11 +0100

    added option to default options

--- src/Sushi.MediaKiwi.Vue/src/router/getDefaultRouterOptions.ts
+++ src/Sushi.MediaKiwi.Vue/src/router/getDefaultRouterOptions.ts
@@ -1,9 +1,10 @@
 import { createWebHistory, type RouteRecordRaw, type RouterOptions } from "vue-router";
 import SignIn from "@/views/SignIn.vue";
 import LoginRedirect from "@/views/LoginRedirect.vue";
+import QueryString from "qs";
 
 /** Creates default router options based on provided modules. */
-export function getDefaultRouterOptions(customRoutes?: RouteRecordRaw[]): RouterOptions {
+export function getDefaultRouterOptions(customRoutes?: RouteRecordRaw[], parseQuery?: boolean): RouterOptions {
   // create routes
   const routes = <RouteRecordRaw[]>[];
 
@@ -25,6 +26,14 @@ export function getDefaultRouterOptions(customRoutes?: RouteRecordRaw[]): Router
 
   const routerOptions = <RouterOptions>{
     routes: routes,
+    parseQuery: function (search: string) {
+      if (!parseQuery) return QueryString.parse(search, { parseArrays: false });
+      return QueryString.parse(search, { parseArrays: true, allowEmptyArrays: true, strictNullHandling: true });
+    },
+    stringifyQuery: function (query: any) {
+      if (!parseQuery) return QueryString.stringify(query, { arrayFormat: "repeat" });
+      return QueryString.stringify(query, { arrayFormat: "brackets", strictNullHandling: true });
+    },
     history: createWebHistory(),
     scrollBehavior: function (to, _from, savedPosition) {
       if (savedPosition) {
