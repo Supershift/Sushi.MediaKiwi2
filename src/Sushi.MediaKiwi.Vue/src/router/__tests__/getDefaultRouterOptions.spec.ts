aef4db35edff4a197a3f8bcedf3d544536495a2e
Luigi Arends <luigi.arends@supershift.nl>
2024-12-24 10:59:05 +0100

    test default router options

---
+++ src/Sushi.MediaKiwi.Vue/src/router/__tests__/getDefaultRouterOptions.spec.ts
@@ -0,0 +1,41 @@
+import "reflect-metadata";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { container } from "tsyringe";
+import { defineComponent } from "vue";
+import { getDefaultRouterOptions } from "../getDefaultRouterOptions";
+
+// mock libraries
+vi.mock("vue-router");
+
+describe("RouteGenerator", () => {
+  beforeEach(() => {
+    container.reset();
+    vi.clearAllMocks();
+  });
+  it("Should return custom routes items", () => {
+    // arrange
+    const routerOptions = getDefaultRouterOptions([{
+      path: "/Test",
+      name: "Test",
+      component: defineComponent({ template: "<div>Test</div>" }),
+    }]);
+
+    // assert
+    expect(routerOptions.routes).toHaveLength(4); // signin, loginRedirect, root, Test
+    expect(routerOptions.routes.find((route) => route.path === "/Test")).toBeDefined();
+  });
+  it("Should return parsed query", () => {
+    // arrange
+    const routerOptions = getDefaultRouterOptions([{
+      path: "/Test",
+      name: "Test",
+      component: defineComponent({ template: "<div>Test</div>" }),
+    }], true);
+
+    const res = routerOptions?.parseQuery ? routerOptions.parseQuery("test[]=1&test[]=2") : {};
+
+    // assert
+    expect(res).toBeDefined(); // signin, loginRedirect, root, Test
+    expect(res?.test?.length).toBe(2);
+  });
+});
