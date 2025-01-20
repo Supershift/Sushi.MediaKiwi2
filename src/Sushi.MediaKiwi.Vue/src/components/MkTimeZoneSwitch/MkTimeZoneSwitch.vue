6c9e0b758b093d5646e5061a025a84d07a4fd830
Koen van der Meer <koen.vandermeer@supershift.nl>
2025-01-20 17:06:26 +0100

    fix error

--- src/Sushi.MediaKiwi.Vue/src/components/MkTimeZoneSwitch/MkTimeZoneSwitch.vue
+++ src/Sushi.MediaKiwi.Vue/src/components/MkTimeZoneSwitch/MkTimeZoneSwitch.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { useTimeZones } from "@/composables/useTimeZones";
-  const { getTimeZones, currentTimeZone, setTimeZone } = useTimeZones();
+  const { timeZones, currentTimeZone, setTimeZone } = useTimeZones();
 
   async function changeTimeZone(timeZone: string) {
     setTimeZone(timeZone);
@@ -8,5 +8,5 @@
 </script>
 
 <template>
-  <v-autocomplete v-model="currentTimeZone" :items="getTimeZones" label="Time zone" item-title="name" item-value="value" @update:model-value="changeTimeZone" />
+  <v-autocomplete v-model="currentTimeZone" :items="timeZones" label="Time zone" item-title="name" item-value="value" @update:model-value="changeTimeZone" />
 </template>
